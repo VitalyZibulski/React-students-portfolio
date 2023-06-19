102a58f2a5e59fbc9580db23725e681d66ddaccc
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-06-19 19:05:26 +0300

    add main

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from  '../../../assets/images/portfolio.jpg';
+
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={'center'} justify={'space-around'}>
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Vitaly Zibulski</Name>
+                    <MainTitle>A Fullstack Developer</MainTitle>
+                </div>
+
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: rgba(236,197,156,0.94);
+`
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+  
+`
+
+const Name = styled.h2`
+  
+`
+
+
