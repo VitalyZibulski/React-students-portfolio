21c30ad4c7555f6a4ef375f1321dafc2b4b85127
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-06-20 10:51:36 +0300

    add section slogan

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Skills } from "./layout/sections/skills/Skills";
 import { Works } from "./layout/sections/works/Works";
 import { Testimony } from "./layout/sections/testimony/Testimony";
 import { Contact } from "./layout/sections/contact/Contact";
+import { Slogan } from "./layout/sections/slogan/Slogan";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Works />
             <Testimony />
             <Contact />
+            <Slogan />
         </div>
     );
 }
